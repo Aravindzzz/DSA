e55b51236cc87b0acafe9429ec23d4ca5dc64e48
Aravind S <113629372+Aravindzzz@users.noreply.github.com>
2024-03-20 13:02:37 +0530

    Time: 546 ms (16.52%) | Memory: 29.7 MB (22.09%) - LeetSync

--- 11-container-with-most-water/container-with-most-water.py
+++ 11-container-with-most-water/container-with-most-water.py
@@ -6,16 +6,12 @@ class Solution:
         while left<=right:
             area = min(height[left],height[right]) * (right-left)
             ans = max(area,ans)
-            if height[left]<height[right]:
+            if height[left]<=height[right]:
                 left+=1
             else:
                 right-=1
         return ans
 
-        f = open('user.out', 'w')
-        for case in map(loads, stdin):
-            f.write(f"{maxArea(case)}\n")
-            f.flush()
-            exit(0)
+       
 
         
