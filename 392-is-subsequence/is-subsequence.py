a54a4e718b91188e343eb55d8e0378ace5451ec0
Aravind S <113629372+Aravindzzz@users.noreply.github.com>
2024-04-09 17:45:33 +0530

    Time: 38 ms (43.45%) | Memory: 16.8 MB (11.79%) - LeetSync

--- 392-is-subsequence/is-subsequence.py
+++ 392-is-subsequence/is-subsequence.py
@@ -5,7 +5,9 @@ class Solution:
         while left <len(s) and right<len(t):
             if s[left] == t[right]:
                 left+=1
-            right+=1
+                right+=1
+            else:
+                right+=1
         return left==len(s)
 
         
